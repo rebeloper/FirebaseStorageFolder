ab4ab100437d4d99f1721124a2e8b8ec5a9b3e49
alexatgetcalvinapp <alex@getcalvin.app>
2025-02-21 15:28:41 +0200

    ImagePicker

---
+++ Sources/FirebaseStorageFolder/ImagePicker.swift
@@ -0,0 +1,44 @@
+//
+//  ImagePicker.swift
+//  FirebaseStorageFolder
+//
+//  Created by Alex Nagy on 21.02.2025.
+//
+
+import SwiftUI
+import PhotosUI
+
+public struct ImagePicker<Label: View>: View {
+    
+    @State private var selectedItems = [PhotosPickerItem]()
+    @State private var selectedImages = [Image]()
+    
+    var maxSelectionCount: Int?
+    var selectionBehavior: PhotosPickerSelectionBehavior
+    var matching: PHPickerFilter?
+    @ViewBuilder var label: @Sendable ([Image]) -> Label
+    
+    public init(maxSelectionCount: Int? = nil, selectionBehavior: PhotosPickerSelectionBehavior = .default, matching: PHPickerFilter? = .images, @ViewBuilder label: @Sendable @escaping ([Image]) -> Label) {
+        self.maxSelectionCount = maxSelectionCount
+        self.selectionBehavior = selectionBehavior
+        self.matching = matching
+        self.label = label
+    }
+    
+    public var body: some View {
+        PhotosPicker(selection: $selectedItems, maxSelectionCount: 1, selectionBehavior: selectionBehavior, matching: matching) { [selectedImages] in
+            label(selectedImages)
+        }
+        .onChange(of: selectedItems) {
+            Task {
+                var selectedImages = [Image]()
+                for item in selectedItems {
+                    if let data = try? await item.loadTransferable(type: Data.self), let uiImage = UIImage(data: data) {
+                        selectedImages.append(Image(uiImage: uiImage))
+                    }
+                }
+                self.selectedImages = selectedImages
+            }
+        }
+    }
+}
